33a598b884800162618c88a1f722517e6025c215
Mc01 <marcin.majchrzak@clabs.co>
2025-06-17 14:20:52 +0200

    Use nested safe simulation.

--- src/improvements/tasks/eth/014-opcm-upgrade-v200-celo/simulate.sh
+++ src/improvements/tasks/eth/014-opcm-upgrade-v200-celo/simulate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SIMULATE_WITHOUT_LEDGER=1 SIGNER_ADDRESS=0xe571b94CF7e95C46DFe6bEa529335f4A11d15D92 just --dotenv-path $(pwd)/.env --justfile ../../../single.just simulate
+SIMULATE_WITHOUT_LEDGER=1 SIGNER_ADDRESS=0xe571b94CF7e95C46DFe6bEa529335f4A11d15D92 just --dotenv-path $(pwd)/.env --justfile ../../../nested.just simulate
