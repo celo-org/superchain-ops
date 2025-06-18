109bd2da02ea20c2fa5eb287d83626c197fe9508
Mc01 <marcin.majchrzak@clabs.co>
2025-06-18 14:49:52 +0200

    Add script to execute nested safe txs.

--- src/improvements/tasks/eth/014-opcm-upgrade-v200-celo/execute.sh
+++ src/improvements/tasks/eth/014-opcm-upgrade-v200-celo/execute.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 [ -z "$SIGNER_PK" ] && echo "Need to set the SIGNER_PK via env" && exit 1;
 
-SIMULATE_WITHOUT_LEDGER=1 PK_OWNER=$SIGNER_PK PK_EXEC=$SIGNER_PK just --dotenv-path $(pwd)/.env --justfile ../../../single.just sign_and_execute_in_anvil 0xf05f102e890E713DC9dc0a5e13A8879D5296ee48
+SIMULATE_WITHOUT_LEDGER=1 PK_OWNER=$SIGNER_PK PK_EXEC=$SIGNER_PK just --dotenv-path $(pwd)/.env --justfile ../../../nested.just sign_and_execute_in_anvil 0x4092A77bAF58fef0309452cEaCb09221e556E112 0xC03172263409584f7860C25B6eB4985f0f6F4636
