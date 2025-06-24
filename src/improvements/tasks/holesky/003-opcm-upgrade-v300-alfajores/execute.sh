aa1c17fec59f6f41f0db1dc6ecefc1a70320856e
Mc01 <marcin.majchrzak@clabs.co>
2025-06-24 14:09:45 +0200

    Upgrade Alfajores.

--- src/improvements/tasks/holesky/003-opcm-upgrade-v300-alfajores/execute.sh
+++ src/improvements/tasks/holesky/003-opcm-upgrade-v300-alfajores/execute.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 [ -z "$SIGNER_PK" ] && echo "Need to set the SIGNER_PK via env" && exit 1;
 
-SIMULATE_WITHOUT_LEDGER=1 PK_OWNER=$SIGNER_PK PK_EXEC=$SIGNER_PK just --dotenv-path $(pwd)/.env --justfile ../../../single.just sign_and_execute_in_anvil 0xd542f3328ff2516443FE4db1c89E427F67169D94
+SIMULATE_WITHOUT_LEDGER=1 PK_OWNER=$SIGNER_PK PK_EXEC=$SIGNER_PK just --dotenv-path $(pwd)/.env --justfile ../../../single.just sign_and_execute_in_anvil 0xf05f102e890E713DC9dc0a5e13A8879D5296ee48
